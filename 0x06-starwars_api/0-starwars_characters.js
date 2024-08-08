8602275bcb4ed369e08f1452777809f18bd4dea9
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-08-08 18:03:32 +0000

    url???

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 // prints all characters of a Star Wars movie.
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
